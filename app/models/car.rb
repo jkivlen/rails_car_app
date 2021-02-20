2efbf5a8cb53e48f77cfa1f1c83cf91b09db699d
jkivlen <jimmykivlen@gmail.com>
2021-02-20 01:43:03 -0600

    added index and show views

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,13 @@
 class Car < ApplicationRecord
+
+    def display_price
+        split_price = self.price.to_s.split(".")
+        if split_price[1].length == 1
+            split_price[1] << "0"
+        elsif split_price[1].length == 0       
+            split_price[1] << "00"
+        end
+        display = split_price.join(".")
+        "$#{display}"
+    end
 end
