6f7743dafd327fe343c0b082ef66acdee6647cbc
jkivlen <jimmykivlen@gmail.com>
2021-02-22 11:49:25 -0600

    added CRUD functionality to cars controller and views

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,8 +1,16 @@
 class CarsController < ApplicationController
+
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
   end
 
   def index
@@ -14,16 +22,29 @@ class CarsController < ApplicationController
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    car = Car.find(params[:id])
+    car.update(car_params[:id])
+    redirect_to car_path(car)
   end
 
   def destroy
+    car = Car.find(params[:id])
+    car.destroy
+    redirect_to cars_path
   end
 
   def expensive
     @car = Car.ordered_by_price.first
   end
   
+  private
+
+  def car_params
+    params.require(:car).permit(:make, :model, :price, :color)
+  end
+
 end
